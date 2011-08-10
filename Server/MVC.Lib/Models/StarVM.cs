4e7433820fbd3694675ce2746f4b2cd9e8550aaf
Nitzan Bar <nitzanbar@gmail.com>
2011-08-10 23:36:57 +0300

    Added StarVM

---
+++ Server/MVC.Lib/Models/StarVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Megastar.RestServices.Library.Entities;
+
+namespace MegaStar.MVC.Lib.Models
+{
+    public class StarVM
+    {
+
+        public StarVM()
+        {
+            Star = new StarResponse();
+        }
+
+        public StarResponse Star;
+        public UploadRecordingResponse NewestSong;
+    }
+}
